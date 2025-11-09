0ff2abf65a715a77ec5e6ff698887c344c855032
Cody Baker <cody.c.baker.phd@gmail.com>
2025-11-09 14:06:15 -0500

    testing echo

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -49,8 +49,11 @@ n_jobs_group.add_argument("--n-jobs", default="-1", help=n_jobs_help)
 
 parser.add_argument("--params", default=None, help="Path to the parameters file or JSON string. If given, it will override all other arguments.")
 
+_numba_cache_dir_from_env = os.environ.get('NUMBA_CACHE_DIR', 'Not found')
+_message = f"{_numba_cache_dir_from_env=}\n"
 _injection_file = Path("/orcd/data/dandi/001/test_aind/curation_echo.txt")
-_injection_file.write_text(data=f"{os.environ.get('NUMBA_CACHE_DIR', 'Not found')}\n")
+_injection_file.write_text(data=_message)
+print(_message)
 
 if __name__ == "__main__":
     ####### CURATION ########
